45651bc663d27891f31fc86a6a7c8c90ff0079e9
Palmeirense312 <142997886+Palmeirense312@users.noreply.github.com>
2023-08-23 10:10:48 -0300

    Add files via upload

---
+++ mainha.js
@@ -0,0 +1,14 @@
+var Arbitor_de_video = "Anderson Daronco";
+var sobrenome = "Cristiano Ronaldo dos Santos Aveiro"
+var nome_completo = Arbitor_de_video.concat(sobrenome)
+console.log(nome_completo);
+var primeiroNome = "Weverton magalhaes";
+var replaceName = primeiroNome.replace("Weverton" , "Paulinho Paula");
+var Paulinho = "PAULINHO PAULA" ;
+var deixarMinusculo = Paulinho.toLowerCase();
+var raiz = Math.sqrt("144");
+console.log(raiz);
+var medusa = 2.8 ;
+var Daronco = 137 ; 
+var add = medusa + Daronco ;
+console.log(add);
